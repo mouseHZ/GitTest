e97f852a07f01de9aa7329d734853a8d1205300b
mouseHZ <985071151@qq.com>
2019-05-19 01:15:03 +0800

    1.修改测试Contoller注释；

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * idea web测试
- *
- * @author: mouse
- * @date: 2019-05-19 00:48
+ * @Author: mouse
+ * @Date: 2019-05-19 00:48
  */
 @RestController
 public class SampleController {
